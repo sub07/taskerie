83f728e137e7e398f65856ccef9d28a5605ab153
Marius Pardo <pardomarius@gmail.com>
2025-04-03 23:01:01 +0200

    deserialize yaml

---
+++ taskerie/src/config.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+
+pub struct Root {
+    pub tasks: HashMap<String, Task>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Task {
+    pub commands: Vec<String>,
+    #[serde(default)]
+    pub on_failure: Vec<String>,
+    #[serde(default)]
+    pub on_success: Vec<String>,
+    #[serde(default)]
+    pub args: HashMap<String, Arg>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Arg {
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub default: Option<String>,
+}
--- taskerie/src/lib.rs
+++ taskerie/src/lib.rs
@@ -1,7 +1,24 @@
+mod config;
+
 use std::path::Path;
 
 pub struct Context {}
 
 pub fn load<P: AsRef<Path>>(path: P) -> anyhow::Result<Context> {
     todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::config::Root;
+
+    use super::*;
+
+    #[test]
+    fn playground() {
+        const CONF: &str = include_str!("../../taskerie.example.yaml");
+
+        let config: Root = serde_norway::from_str(CONF).unwrap();
+        println!("{config:#?}");
+    }
 }
